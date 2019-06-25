d48ca1b58696683d16f07db332deb4f5f4692f05
Natalia Selezneva <natalia.selezneva@jetbrains.com>
2019-06-25 16:22:45 +0300

    Scripts: pass full path finding script definition instead of file name

--- plugins/scripting/scripting-compiler/src/org/jetbrains/kotlin/scripting/compiler/plugin/JvmCliScriptEvaluationExtension.kt
+++ plugins/scripting/scripting-compiler/src/org/jetbrains/kotlin/scripting/compiler/plugin/JvmCliScriptEvaluationExtension.kt
@@ -21,7 +21,6 @@ import org.jetbrains.kotlin.config.CompilerConfiguration
 import org.jetbrains.kotlin.config.JVMConfigurationKeys
 import org.jetbrains.kotlin.scripting.configuration.ScriptingConfigurationKeys
 import org.jetbrains.kotlin.scripting.definitions.ScriptDefinitionProvider
-import org.jetbrains.kotlin.scripting.definitions.StandardScriptDefinition
 import java.io.File
 
 class JvmCliScriptEvaluationExtension : ScriptEvaluationExtension {
@@ -41,7 +40,7 @@ class JvmCliScriptEvaluationExtension : ScriptEvaluationExtension {
         }
         val sourcePath = arguments.freeArgs.first()
         val scriptFile = File(sourcePath)
-        if (scriptFile.isDirectory || !scriptDefinitionProvider.isScript(scriptFile.name)) {
+        if (scriptFile.isDirectory || !scriptDefinitionProvider.isScript(scriptFile.path)) {
             val extensionHint =
                 if (configuration.get(ScriptingConfigurationKeys.SCRIPT_DEFINITIONS)?.let { it.size == 1 && it.first().isDefault } == true) " (.kts)"
                 else ""
