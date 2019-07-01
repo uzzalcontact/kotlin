6f7b65b61b46af7e642d5afd818a3ed3553c86eb
Igor Yakovlev <igor.yakovlev@jetbrains.com>
2019-07-01 18:07:12 +0300

    Fix UL-classes test renders and test data

--- compiler/testData/asJava/ultraLightClasses/delegatesWithAnnotations.java
+++ compiler/testData/asJava/ultraLightClasses/delegatesWithAnnotations.java
@@ -1,11 +1,11 @@
-@java.lang.annotation.Retention(null=java.lang.annotation.RetentionPolicy.RUNTIME)
+@java.lang.annotation.Retention(value = java.lang.annotation.RetentionPolicy.RUNTIME)
 public abstract @interface SimpleAnn /* SimpleAnn*/ {
   @null()
   public abstract java.lang.String value();
 
 }
 
-@java.lang.annotation.Retention(null=java.lang.annotation.RetentionPolicy.RUNTIME)
+@java.lang.annotation.Retention(value = java.lang.annotation.RetentionPolicy.RUNTIME)
 public abstract @interface Ann /* Ann*/ {
   @null()
   public abstract SimpleAnn[] t();
@@ -27,14 +27,14 @@ public abstract @interface Ann /* Ann*/ {
 }
 
 public abstract interface Base /* Base*/ {
-  @Ann(null=1, null="134", null=String::class, null=arrayOf(Int::class, Double::class), null=DeprecationLevel.WARNING, null=SimpleAnn("243"), null=SimpleAnn("4324"))
-  public abstract void foo(@Ann(null=2, null="324", null=Ann::class, null=arrayOf(Byte::class, Base::class), null=DeprecationLevel.WARNING, null=SimpleAnn("687"), null=SimpleAnn("78")) @org.jetbrains.annotations.NotNull() java.lang.String);
+  @Ann(x = 1, y = "134", z = String::class, e = arrayOf(Int::class, Double::class), depr = DeprecationLevel.WARNING, t = @Ann(1, "134", String::class, arrayOf(Int::class, Double::class), DeprecationLevel.WARNING, SimpleAnn("243"), SimpleAnn("4324")))
+  public abstract void foo(@Ann(x = 2, y = "324", z = Ann::class, e = arrayOf(Byte::class, Base::class), depr = DeprecationLevel.WARNING, t = @Ann(2, "324", Ann::class, arrayOf(Byte::class, Base::class), DeprecationLevel.WARNING, SimpleAnn("687"), SimpleAnn("78"))) @org.jetbrains.annotations.NotNull() java.lang.String);
 
 }
 
 public final class Derived /* Derived*/ implements Base {
-  @Ann(x=1, y="134", z=java.lang.String.class, e={int.class, double.class}, depr=kotlin.DeprecationLevel.WARNING, t={@SimpleAnn(value="243"), @SimpleAnn(value="4324")})
-  public void foo(@Ann(x=2, y="324", z=Ann.class, e={byte.class, Base.class}, depr=kotlin.DeprecationLevel.WARNING, t={@SimpleAnn(value="687"), @SimpleAnn(value="78")}) @org.jetbrains.annotations.NotNull() java.lang.String);
+  @Ann(x = 1, y = "134", z = java.lang.String.class, e = {int.class, double.class}, depr = kotlin.DeprecationLevel.WARNING, t = {@SimpleAnn(value="243"), @SimpleAnn(value="4324")})
+  public void foo(@Ann(x = 2, y = "324", z = Ann.class, e = {byte.class, Base.class}, depr = kotlin.DeprecationLevel.WARNING, t = {@SimpleAnn(value="687"), @SimpleAnn(value="78")}) @org.jetbrains.annotations.NotNull() java.lang.String);
 
   @null()
   public  Derived(@org.jetbrains.annotations.NotNull() Base);
